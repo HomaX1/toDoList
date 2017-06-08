5d4d51b018f762d42a6a3a0a451dde3864fe667e
HomaX1 <baitasova.anastasiia@gmail.com>
2017-06-08 17:31:46 +0300

    add category on the page with js

--- js/ajax.js
+++ js/ajax.js
@@ -1,5 +1,14 @@
-$.get("../restore.json", function(data, status){
-    console.log("Data: " + data + "\nStatus: " + status);
+$.get("../restore.json", (data, status) => {
+
+    let categoryUl = $('.category');
+
+    for (let i = 0; i < data.length; ++i) {
+        let newLi = document.createElement('li');
+        newLi.innerHTML = data[i].name;
+        categoryUl.append(newLi);
+    }
+
+   /* console.log("\nStatus: " + status);*/
 });
 
 
