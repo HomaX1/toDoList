98545c7043c0c65d0524360293b6edf99f97dafe
HomaX1 <baitasova.anastasiia@gmail.com>
2017-06-19 22:51:04 +0300

    change the view of code

--- js/script.js
+++ js/script.js
@@ -1,120 +1,83 @@
-/*
- $(document).ready(function () {
+$(function () {
+    const ALLCONST = {
+        categoryUl: $('.category'),
+        tasksUl: $('.tasks'),
+        btnAddCategory: $('.adding-form__button_category'),
+        btnAddTask: $('.adding-form__button_task')
+    };
+    let globalStorage = [];
+    let idCategory = "";
 
- $.get("../restore.json", (data, status) => {
 
- let categoryUl = $('.category');
- let tasksUl = $('.tasks');
+    let showCategories = (data) => {
+        globalStorage = data;
 
- for (let i = 0; i < data.length; ++i) {
+        let allLi = globalStorage.map(categoryItem => {
+            return "<li data-id-category='" + categoryItem.id + "'>" + categoryItem.name + "</li>";
+        });
 
- let categoryLi = document.createElement('li');
-
- categoryLi.innerHTML = data[i].name;
- categoryUl.append(categoryLi);
-
- categoryLi.onclick = () => {
- tasksUl.css('visibility', 'visible');
- $('.tasks li').hide();
-
- for (let j = 0; j < data[i].list.length; ++j) {
- let tasksLi = document.createElement('li');
- tasksLi.innerHTML = data[i].list[j];
- tasksUl.append(tasksLi);
- }
-
- };
- }
-
- /!* console.log("\nStatus: " + status);*!/
- });
-
- });
-
- */
-
-$(document).ready(function () {
-    let storageCategory = [];
-    let categoryUl = $('.category');
-    let tasksUl = $('.tasks');
-
-    let showCategory = (data) => {
-        let allLi = "";
-        storageCategory = data;
-
-        for (let i = 0; i < storageCategory.length; i++) {
-            allLi += "<li data-id-category='" + storageCategory[i].id + "'>" + storageCategory[i].name + "</li>";
-        }
-
-        categoryUl.append(allLi);
-        categoryUl.find('li').on('click', showTasks);
+        ALLCONST.categoryUl.append(allLi);
+        ALLCONST.categoryUl.find('li').on('click', showTasks);
     };
 
-    /*data-lists='" + storageCategory[i].list + "'*/
 
-    let btnAddCategory = $('.adding-form__button_category');
     let addCategory = (e) => {
-        e.preventDefault();
-        categoryUl.empty();
-
-        let idDate = new Date();
-        let adding = $('.adding-form__input').val();
-        let viewAdding = {
+        const idDate = new Date();
+        const adding = $('.adding-form__input').val();
+        const viewAdding = {
             name: adding,
             id: idDate.getTime(),
             list: []
         };
 
-        /*console.log(idDate.getTime());*/
+        e.preventDefault();
+        ALLCONST.categoryUl.empty();
 
-        storageCategory.push(viewAdding);
-        showCategory(storageCategory);
+        globalStorage.push(viewAdding);
+        showCategories(globalStorage);
     };
 
-    btnAddCategory.click(addCategory);
 
-
-    let idCategory = "";
     let showTasks = (element) => {
-        if(element) {
+        if (element) {
             window.location.hash = element.currentTarget.dataset.idCategory;
         }
 
         idCategory = +window.location.hash.substring(1) || element.currentTarget.dataset.idCategory;
 
-        for (let j = 0; j < storageCategory.length; j++) {
-            if (+idCategory === storageCategory[j].id) {
+        for (let j = 0; j < globalStorage.length; j++) {
+            if (+idCategory === globalStorage[j].id) {
 
-                let liTask = storageCategory[j].list.map(task => {
+                let liTask = globalStorage[j].list.map(task => {
                     return "<li>" + task + "</li>";
                 });
 
-                tasksUl.empty();
-                tasksUl.append(liTask);
+                ALLCONST.tasksUl.empty();
+                ALLCONST.tasksUl.append(liTask);
 
             }
         }
     };
 
 
-    let btnAddTask = $('.adding-form__button_task');
     let addTask = (e) => {
-        e.preventDefault();
-        let addingTask = $('.adding-form__input_addTask').val();
+        const addingTask = $('.adding-form__input_addTask').val();
 
-        storageCategory.forEach(function(item) {
+        e.preventDefault();
+
+        globalStorage.forEach(function (item) {
             if (+idCategory === item.id) {
                 item.list.push(addingTask);
                 showTasks();
-                console.log(storageCategory);
             }
         });
     };
 
-    btnAddTask.click(addTask);
 
+    ALLCONST.btnAddCategory.click(addCategory);
+    ALLCONST.btnAddTask.click(addTask);
 
-    $.get("../restore.json", showCategory);
+    $.get("../restore.json", showCategories);
 
 });
 
